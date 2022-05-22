83f9f66e741cdaebc720b039a35426a536e98bfb
Navanjana <104069319+Captainamarica@users.noreply.github.com>
2022-05-22 12:36:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-#    This file is part of the DARK EMPIRE distribution (https://github.com/DARKEMPIRESL/Telegraph-Uploader).
+#    This file is part of the DARK EMPIRE distribution (https://github.com/Captainamarica/NightVission-Telegrapbot).
 #    Copyright (c) 2021 Rithunand
 #    
 #    This program is free software: you can redistribute it and/or modify  
